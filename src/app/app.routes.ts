636d336f1a81b0f0ec754dc1f3648f38f2923902
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-01-19 02:39:41 -0500

    Agregue la ruta para el administrador principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
+import { PrincipalAdministrador } from './domains/principal-administrador/principal-administrador.component';
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
 
+    {
+      path: 'principalAdministrador',
+      component: PrincipalAdministrador,
+    },
 
 ];
