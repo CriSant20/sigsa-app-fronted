b010e363cce6e624f32128ea31894efa37099267
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-01-19 02:38:05 -0500

    Cree el modulo raíz para agregar componentes y modulos

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrincipalAdministrador } from './domains/principal-administrador/principal-administrador.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalAdministrador
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,  // Asegúrate de que CommonModule esté importado
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
