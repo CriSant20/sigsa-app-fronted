e3964862510053d693bc935df0a0bc8ace9df28b
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-01-19 19:05:48 -0500

    Moví mi carpeta del cliente principal a la carpeta Pages y elimine la carpeta domains

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { PrincipalAdministrador } from './Pages/principal-administrador/principal-administrador.component';
-import { PrincipalCliente } from './domains/principal-cliente/principal-cliente.component';
+import { PrincipalCliente } from './Pages/principal-cliente/principal-cliente.component';
 
 export const routes: Routes = [
 
