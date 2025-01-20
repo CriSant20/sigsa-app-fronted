3d16f2d4861ed81894384b43d4af052b384d4999
CriSant20 <92830667+CriSant20@users.noreply.github.com>
2025-01-19 19:25:39 -0500

    se crea la rama pantalla-gestion-administrador con la finalidad de manejar las solicitudes de clientes, se crea el componente

---
+++ src/app/Pages/pantalla-gestion-administrador/pantalla-gestion-administrador.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PantallaGestionAdministradorComponent } from './pantalla-gestion-administrador.component';
+
+describe('PantallaGestionAdministradorComponent', () => {
+  let component: PantallaGestionAdministradorComponent;
+  let fixture: ComponentFixture<PantallaGestionAdministradorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PantallaGestionAdministradorComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PantallaGestionAdministradorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Pages/pantalla-gestion-administrador/pantalla-gestion-administrador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-gestion-administrador',
+  imports: [],
+  templateUrl: './pantalla-gestion-administrador.component.html',
+  styleUrl: './pantalla-gestion-administrador.component.css'
+})
+export class PantallaGestionAdministradorComponent {
+
+}
