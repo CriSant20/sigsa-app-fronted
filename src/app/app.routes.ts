99cafc19b1ed97dcaa60334750c7e40517a795b7
ReneeeeeDev <124427046+ReneeeeeDev@users.noreply.github.com>
2025-01-20 17:29:11 -0500

    Diseno de Pagina registro terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
+import { RegistroComponent } from './Pages/registro/registro.component';
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
 
+    { path: 'registro', component: RegistroComponent },
 
 ];
