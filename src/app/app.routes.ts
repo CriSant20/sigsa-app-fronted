42f44afd80ea9a58811935b790d08fa3eda736c4
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-01-20 20:56:48 -0500

    Agregue la ruta para el cliente principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
+import { PrincipalCliente } from './Pages/principal-cliente/principal-cliente.component';
 
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
-    
+
+    {
+        path: 'principalCliente',
+        component: PrincipalCliente,
+      },
 
 ];
