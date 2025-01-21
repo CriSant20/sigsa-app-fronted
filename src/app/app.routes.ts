36b24bf1ae14b169828162aca7337634973d00c8
ReneeeeeDev <124427046+ReneeeeeDev@users.noreply.github.com>
2025-01-21 00:27:06 -0500

    Primer commit de cargar Componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { PrincipalCliente } from './Pages/principal-cliente/principal-cliente.component';
+import { RegistroComponent } from './Pages/registro/registro.component';
 
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent },
 
     {
         path: 'principalCliente',
