1212c984c9da4616c4ddaef12093d1253d70239f
ReneeeeeDev <124427046+ReneeeeeDev@users.noreply.github.com>
2025-01-21 03:36:29 -0500

    componente de Cuadro para subir el comprobante

--- src/app/Pages/registro/registro.component.ts
+++ src/app/Pages/registro/registro.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-registro',
   imports: [],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrl: './registro.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom    // Desactiva el encapsulamiento de estilos
+
 })
 export class RegistroComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { PrincipalCliente } from './Pages/principal-cliente/principal-cliente.component';
 import { RegistroComponent } from './Pages/registro/registro.component';
+import { SubirComprobanteComponent } from './Components/subir-comprobante/subir-comprobante.component';
 
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
+    { path: 'subir', component: SubirComprobanteComponent },
 
     {
         path: 'principalCliente',
