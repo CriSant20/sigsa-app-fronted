95b502508b630a059127213e371c830f9c2caae9
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-01-23 16:43:40 -0500

    Se crea el comentario service y la interfaz

---
+++ src/app/Core/Services/comentarios.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comentario } from '../Interfaces/tarea.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComentariosService {
+    private http = inject(HttpClient);
+    private apiUrl = 'http://localhost:3000/api';
+
+  constructor() { }
+
+  getComentariosByTarea(id: number):Observable<Comentario[]> {
+    return this.http.get<Comentario[]>(`${this.apiUrl}/comentarios/tarea/${id}`);
+  }
+}
